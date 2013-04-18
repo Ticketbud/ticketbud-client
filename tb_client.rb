4be459f69ec53a209ada67d693ef2be05848c57c
Brandon Harris <brandon@brandon-harris.com>
2013-04-17 22:59:16 -0500

    made it a little more generic

--- tb_client.rb
+++ tb_client.rb
@@ -3,11 +3,16 @@ require 'slim'
 require 'oauth2'
 
 CONFIG = YAML.load_file("ticketbud.yml")
+# This should never make it into production code! This makes it easier to prototype this client with
+# a self-signed ssl
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+set :base_domain, CONFIG['base_domain']
 
 get '/' do
   client = OAuth2::Client.new(CONFIG['app_id'], CONFIG['secret'], site: CONFIG['site'])
   @authorize_url = client.auth_code.authorize_url(redirect_uri: CONFIG['callback_url'])
-  slim :index
+  slim :index, locals: {callback_url: CONFIG['callback_url'], secret: CONFIG['secret'], app_id: CONFIG['app_id']}
 end
 
 get '/auth/todo/callback' do
