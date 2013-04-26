9574a63f61427b45d0045bbc9bfbf2fbd8818d0c
Brandon Harris <brandon@brandon-harris.com>
2013-04-26 16:33:15 -0500

    working ember example

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,77 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  rootElement: "#ember-mount"
+});
+
+App.Store = DS.Store.extend({
+  revision: 12,
+  adapter: DS.RESTAdapter.create({
+    url: 'https://api.ticketbud.com',
+    ajax: function (url, type, hash) {
+      hash.url = url;
+      hash.type = type;
+      hash.dataType = 'jsonp';
+      hash.contentType = 'application/json; charset=utf-8';
+      hash.context = this;
+
+      if (hash.data) {
+        hash.data['access_token'] = $("meta[name='access_token']").attr('content');
+      } else {
+        hash.data = {
+          access_token: $("meta[name='access_token']").attr('content')
+        };
+      }
+
+      if (hash.data && type !== 'GET') {
+        hash.data = JSON.stringify(hash.data);
+      }
+
+      $.ajax(hash);
+    }
+  })
+});
 
 App.Router.map(function() {
-  this.resource('user', {path: '/users/:user_id'});
+  this.resource('events', function() {
+    this.resource('event', {path: ':event_id'}, function(){
+      this.resource('tickets');
+    });
+  });
 });
 
-App.UserRoute = Ember.Route.extend({
+App.EventsRoute = Ember.Route.extend({
   model: function(params) {
-    return users[params.user_id];
+    return App.Event.find();
   }
 });
 
-App.ApplicationRoute = Ember.Route.extend({
-  model: function() {
-    return users;
+App.TicketsRoute = Ember.Route.extend({
+  model: function(params) {
+    var event = this.modelFor('event');
+    return App.Ticket.find({event_id: event.get('id')});
   }
 });
 
-var users = [
-  {
-    id: 1,
-    first: 'Ryan',
-    last: 'Florence',
-    avatar: 'https://si0.twimg.com/profile_images/3123276865/5c069e64eb7f8e971d36a4540ed7cab2.jpeg'
-  },
-  {
-    id: 2,
-    first: 'Tom',
-    last: 'Dale',
-    avatar: 'https://si0.twimg.com/profile_images/1317834118/avatar.png'
-  },
-  {
-    id: 3,
-    first: 'Yehuda',
-    last: 'Katz',
-    avatar: 'https://si0.twimg.com/profile_images/3250074047/46d910af94e25187832cb4a3bc84b2b5.jpeg'
-  }
-];
+App.EventsController = Ember.ArrayController.extend({
+});
+
+App.TicketsController = Ember.ArrayController.extend({});
+
+App.Event = DS.Model.extend({
+  title: DS.attr('string'),
+  eventStart: DS.attr('date'),
+  eventEnd: DS.attr('date'),
+  timeZone: DS.attr('string'),
+  image: DS.attr('string'),
+  ticketsAvailable: DS.attr('number'),
+  soldOut: DS.attr('boolean'),
+  over: DS.attr('boolean'),
+  tickets: DS.hasMany('App.Ticket')
+});
+
+App.Ticket = DS.Model.extend({
+  nameOnTicket: DS.attr('string'),
+  checkedIn: DS.attr('boolean'),
+  barcodeUrl: DS.attr('string'),
+  ticketType: DS.attr('string'),
+  event: DS.belongsTo('App.Event')
+});
